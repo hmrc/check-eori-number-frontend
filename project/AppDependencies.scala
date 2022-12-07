68feba98a8e8cfb65d91fe483a33b4b638b297dd
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2022-12-07 11:52:50 +0000

    DLS-6476 play upgrade (#37)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,13 +5,13 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "6.4.0"
+  val bootstrapVersion = "7.12.0"
   val playVersion = "play-28"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"          % "3.22.0-play-28",
-    "uk.gov.hmrc"             %% "play-language"               % "5.3.0-play-28"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"          % "3.34.0-play-28",
+    "uk.gov.hmrc"             %% "play-language"               % "5.4.0-play-28"
   )
 
   val test = Seq(
