a6a74bdb89d88e3441401204954b4f287c5c7329
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2026-02-13 16:30:13 +0000

    DDCYLS-8090: Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,17 @@ object AppDependencies {
 
   val bootstrapVersion = "10.5.0"
   val playVersion      = "play-30"
-  val mongoVersion     = "2.11.0"
+  val mongoVersion     = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion" % bootstrapVersion,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion" % "12.26.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"         % mongoVersion
+    "uk.gov.hmrc"         %% s"bootstrap-frontend-$playVersion" % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
+    "uk.gov.hmrc"         %% s"play-frontend-hmrc-$playVersion" % "12.26.0",
+    "at.yawk.lz4"         %  "lz4-java"                         % "1.10.3",
+    "org.mozilla"         % "rhino"                             % "1.8.1",
+    "ch.qos.logback"      % "logback-core"                      % "1.5.27",
+    "ch.qos.logback"      % "logback-classic"                   % "1.5.27",
+    "org.apache.commons"  % "commons-lang3"                     % "3.18.0",
+    "uk.gov.hmrc.mongo"   %% s"hmrc-mongo-$playVersion"         % mongoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
